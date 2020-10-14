f922a051fb2101f52fc1d63e05d804814545c36e
Filip Mularski <mularskif@gmail.com>
2020-10-14 12:43:39 +0200

    sliders colorful backgrounds

--- app.js
+++ app.js
@@ -20,10 +20,51 @@ function generateHex() {
 
 function fillDivsWithColors(){
     colorDivs.forEach( div => {
-        let generatedColor = generateHex();
-        div.children[0].innerText = generatedColor;
+        const generatedColor = generateHex();
+        const hexH2 = div.children[0]; 
+        
+        hexH2.innerText = generatedColor;
         div.style.backgroundColor = generatedColor;
+
+        checkTextContrast(generatedColor, hexH2);
+
+        // initial color sliders
+
+        const color = chroma(generatedColor);
+        const sliders = div.querySelectorAll('.sliders input');
+        
+        const hue = sliders[0];
+        const brightness = sliders[1];
+        const saturation = sliders[2];
+
+        colorizeSliders(color, hue, brightness, saturation);
+
     })
+}
+
+function checkTextContrast(color, text) {
+    const luminance = chroma(color).luminance();
+
+    if ( luminance > 0.5)
+        text.style.color = "black";
+    else 
+        text.style.color = "white";
+}
+
+function colorizeSliders(color, hue, brightness, saturation) {
+    /* saturation */
+    const noSaturation = color.set('hsl.s', 0);
+    const fullSaturation = color.set('hsl.s', 1);
+    const scaleSaturation = chroma.scale([noSaturation, color, fullSaturation]);
+    saturation.style.backgroundImage = `linear-gradient(to right, ${scaleSaturation(0)}, ${scaleSaturation(1)})`;
+
+    /* brightness */
+    const midBrightness = color.set('hsl.l', 0.5);
+    const scaleBrightness = chroma.scale(["black", midBrightness, "white"]);
+    brightness.style.backgroundImage = `linear-gradient(to right, ${scaleBrightness(0)}, ${scaleBrightness(0.5)}, ${scaleBrightness(1)})`;
+
+    /* hue */ 
+    hue.style.backgroundImage = `linear-gradient(to right, rgb(204, 75, 75), rgb(204,204 ,75),rgb(75, 204, 75),rgb(75, 204, 204),rgb(75,75,204),rgb(204,75,204),rgb(204,75,75))`;
 }
 
 fillDivsWithColors();
